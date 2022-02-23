0f310edcb982a6bd9319fc5f9d41675aec48d964
hector <hectoreddbenitez@gmail.com>
2022-02-23 16:58:52 -0300

    Criação e modificações em productsServices.js

---
+++ services/productsServices.js
@@ -0,0 +1,78 @@
+const productModels = require('../models/productsModels');
+const productExist = require('../middlewares/productExist');
+
+const create = async (name, quantity) => {
+if (await productExist(name)) {
+  return { 
+    codigo: 409,
+    message: { message: 'Product already exists' },
+   };
+}
+const product = await productModels.create(name, quantity);
+  return {
+  codigo: 201,
+  message: product,
+  };
+};
+
+const findAll = async () => {
+  const productsList = await productModels.findAll();
+  return {
+    codigo: 200,
+    message: productsList,
+   };
+  };
+
+const findById = async (id) => {
+  const result = await productModels.findById(id);
+   if (result.length === 0) {
+    return { 
+      codigo: 404,
+      message: { message: 'Product not found' },
+    };
+   }
+    return {
+      codigo: 200,
+      message: result[0],
+    };
+};
+
+const updateProduct = async (id, name, quantity) => {
+  const result = await productModels.findById(id);
+  if (result.length === 0) {
+    return { 
+      codigo: 404,
+      message: { message: 'Product not found' },
+    };
+  }
+
+  await productModels.updateProduct(id, name, quantity);
+  return {
+    codigo: 200,
+    message: { id, name, quantity },
+  };
+};
+
+const deleteProduct = async (id) => {
+  const result = await productModels.findById(id);
+  if (result.length === 0) {
+    return { 
+      codigo: 404,
+      message: { message: 'Product not found' },
+    };
+  }
+
+  await productModels.deleteProduct(id);
+  return {
+    codigo: 204,
+    message: result[0],
+  };
+};
+
+module.exports = {
+  create,
+  findAll,
+  findById,
+  updateProduct,
+  deleteProduct,
+};
