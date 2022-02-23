698b895c6b796a98337d3e508715d00417c2ffc3
hector <hectoreddbenitez@gmail.com>
2022-02-23 16:54:00 -0300

    Criação e modificações em productsControllers.js

---
+++ controllers/productsControllers.js
@@ -0,0 +1,39 @@
+const productService = require('../services/productsServices');
+
+const create = async (req, res) => {
+ const { name, quantity } = req.body;
+ const response = await productService.create(name, quantity);
+ return res.status(response.codigo).json(response.message);
+};
+
+const findAll = async (_req, res) => {
+  const response = await productService.findAll();
+  return res.status(response.codigo).json(response.message);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const response = await productService.findById(id);
+  return res.status(response.codigo).json(response.message);
+  };
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, quantity } = req.body;
+  const response = await productService.updateProduct(id, name, quantity);
+  return res.status(response.codigo).json(response.message);
+    };
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  const response = await productService.deleteProduct(id);
+  return res.status(response.codigo).json(response.message);
+};
+
+module.exports = {
+create,
+findAll,
+findById,
+updateProduct,
+deleteProduct,
+};
